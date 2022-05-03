4ad92ea0e348aab95cdbaaefdd5a42480bd0e57b
Elton Freitas <xelton207@gmail.com>
2022-05-03 01:32:09 -0300

    Finishing navMenu

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -11,9 +11,9 @@ const Header = (props) => {
         <img src={logo} alt="logo disney" />
       </Logo>
       <NavMenu>
-        <a href="/">
-          <img src={homeIcon} alt="home Icon"/>
-        </a>
+        <Link to="/">
+          <img src={homeIcon} alt="home Icon" />
+        </Link>
         <span to="/">HOME</span>
       </NavMenu>
     </Nav>
@@ -61,11 +61,57 @@ const NavMenu = styled.div`
   margin-right: auto;
   margin-left: 25px;
 
-  span{
-      cursor: pointer;
-  }
+  
+  a {
+      display: flex;
+      align-items: center;
+      padding: 0 12px;
+    }
 
- /* @media (max-width: 768px) {
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
+    }
+    
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      line-height: 1.88;
+      padding: 2px 0px;
+      white-space: nowrap;
+      position: relative;
+    
+
+    &:before {
+        background-color: rgb(249,249, 249);
+        border-radius: 0px 0px 4px 4px;
+        bottom: -6px;
+        content: '';
+        height: 2px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        left: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+    }
+}
+
+    &:hover {
+        span:before{
+            transform: scaleX(1);
+            visibility: visible;
+            opacity: 1 !important;
+        }
+    }
+
+   /*@media (max-width: 768px) {
     display: none;
   }*/
 `;
